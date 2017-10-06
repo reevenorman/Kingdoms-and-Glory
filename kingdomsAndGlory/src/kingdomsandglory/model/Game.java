022d7e79a13fe6616b00500caf4b539750824d8e
reevenorman <32525093+reevenorman@users.noreply.github.com>
2017-10-06 13:36:18 -0600

    10/6/17 - Initial Commit to repository

---
+++ kingdomsAndGlory/src/kingdomsandglory/model/Game.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kingdomsandglory.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author reeve
+ */
+public class Game implements Serializable{
+       private Long Time;
+       private Long Date;
+       private 
+}
