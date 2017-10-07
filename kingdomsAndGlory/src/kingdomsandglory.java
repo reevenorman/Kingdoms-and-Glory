68af18ee08e404b2cd9130547148d1a60b3eada0
reevenorman <32525093+reevenorman@users.noreply.github.com>
2017-10-06 21:17:37 -0600

    10/6/17 - 9pm david's changes

---
+++ kingdomsAndGlory/src/kingdomsandglory.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author reeve
+ */
+public class kingdomsandglory {
+    
+    
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+        
+    }
+    
+}
