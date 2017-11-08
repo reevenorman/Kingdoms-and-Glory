5d23129084b227e0e95d2c635c4b28ff61a6cb64
pianoace1315 <32525128+pianoace1315@users.noreply.github.com>
2017-11-08 13:48:13 -0700

    11/8 Removing Static and resolving issues

--- kingdomsAndGlory/src/kingdomsandglory/control/GameControl.java
+++ kingdomsAndGlory/src/kingdomsandglory/control/GameControl.java
@@ -5,10 +5,35 @@
  */
 package kingdomsandglory.control;
 
+import kingdomsandglory.model.Player;
+import kingdomsandglory.model.Player.setPlayerName;
+
 /**
  *
- * @author piano
+ * @author reeve
  */
 public class GameControl {
+
+    public static Player savePlayer(String playerName) {
+        if (playerName == null || playerName.length() < 1) {
+            return null;
+        }
+        Player player = new Player();
+        setPlayerName(playerName);
+      
+        return player;
+    }
+
+    public GameControl(String name) {
+        System.out.println("*** savePlayer() called ***");
+    }
+
+    public static void createNewGame(Player player) {
+        System.out.println("***Game Control - createNewGame() Called***");
+    }
     
+    public static void restartGame() {
+        kingdomsandglory.view.StartExistingGameView savedGame = new kingdomsandglory.view.StartExistingGameView();
+        savedGame.displayStartExistingGameView();
+    }
 }
