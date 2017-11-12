0d9ca62c36ed47a5077dbc0978cfade43adf6200
reevenorman <32525093+reevenorman@users.noreply.github.com>
2017-11-11 17:54:46 -0700

    11/11/17	Work

---
+++ kingdomsAndGlory/src/kingdomsglory/kingdomsandgloryMain.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kingdomsglory;
+
+import kingdomsandglory.model.Game;
+import kingdomsandglory.model.Player;
+import kingdomsandglory.view.StartProgramView;
+
+/**
+ *
+ * @author piano
+ */
+public class kingdomsandgloryMain {
+    
+    public void main(String[] args) {
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.display();
+        
+    }
+   
+
+    private static Player player = null;
+    private static Game currentGame = null;
+
+    public static Player getPlayer() {
+        return player;
+    }
+
+    public static void setPlayer(Player player) {
+        kingdomsandgloryMain.player = player;
+    }
+
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        kingdomsandgloryMain.currentGame = currentGame;
+    }
+
+
+    
+}
